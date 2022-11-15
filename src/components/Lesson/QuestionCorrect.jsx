bc344a6e654b983d8d136d614ec568fc946d1c0b
rossw01 <56947241+rossw01@users.noreply.github.com>
2022-11-15 01:25:19 +0000

    Question bg now gets set dynamically on answer submission #30

--- src/components/Lesson/QuestionCorrect.jsx
+++ src/components/Lesson/QuestionCorrect.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import "./QuestionResult.css";
 
 const QuestionCorrect = () => {
-	return (
-		<div className="correct-background">
-			<p>Correct!</p>
-		</div>
-	);
+  return (
+    <div>
+      <p>Correct!</p>
+    </div>
+  );
 };
 
 export default QuestionCorrect;
